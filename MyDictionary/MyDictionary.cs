f7253ee3b5a6c0e782eaf8daae76ce072cb639ad
nerex127 <68194203+nerex127@users.noreply.github.com>
2021-07-14 04:47:13 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+
+        public void Add(K id,V name)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length+1];
+            values = new V[values.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = id;
+            values[values.Length - 1] = name;
+        }
+
+        
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+        public void ShowMyDictionaryList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Id: " + keys[i] + "  " + "Name: " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> customers = new MyDictionary<int, string>();
+            customers.Add(1,"Muhammed Uğur Koçak");
+            customers.Add(2,"Ege Tosun");
+            customers.Add(3, "Uğur Koçak");
+            customers.ShowMyDictionaryList();
+
+        }
+    }
+}
